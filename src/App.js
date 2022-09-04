e727bc2f9a0fe05145f009f0cde3a396ba05a7b4
jakegreenbergbell <32340594+jakegreenbergbell@users.noreply.github.com>
2022-09-03 19:35:23 -0700

    Make logginto airtable.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
 import './App.css';
 import background from "./bg.png"
+import { useEffect, useState } from 'react';
+import axios from 'axios'
+import Airtable from 'airtable'
+
 
 function App() {
+
+  const [email, setEmail] = useState("");
+  const [emaiLCreated, setEmailCreated] = useState(false);
+  axios.defaults.headers.post['Content-Type'] = 'application/json';
+  axios.defaults.headers['Authorization'] = 'Bearer API_KEY';
+
+
+  const sendEmail = async () => {
+    console.log(process.env.REACT_APP_AIRTABLE)
+    const base = new Airtable({apiKey: process.env.REACT_APP_AIRTABLE}).base('appgdBKpbthH8ffmP'); 
+        base('Teams').create([
+          {
+            "fields": {
+              "Name": email,
+            }
+          },
+        ], function(err, records) {
+          if (err) {
+            console.error(err);
+            return;
+          }
+          setEmailCreated(true);
+        });
+  }
+
+  const uploadEmail = async (email) => {
+   console.log(email);
+   await sendEmail(email);
+  };
+
   return (
     <div id="page-container">
       <img id="background" src={background} alt="gradient background with star drawings"/>
@@ -23,9 +57,12 @@ function App() {
           </div>
           <div id="email-box">
             <div>
-              <input id="email-input" type="text" placeholder="Enter Email" className="montserrat"></input>
-              <button id="email-button" className="montserrat">Tune in!</button>
+              <input id="email-input" type="text" placeholder="Enter Email" className="montserrat" onChange={(v) => setEmail(v.target.value)} value={email} />
+              <button id="email-button" className="montserrat" onClick={uploadEmail}>Tune in!</button>
             </div>
+          </div>
+          <div id="thank-you">
+            {emaiLCreated && <span >Thank you! We will be in touch.</span>}
           </div>
         </div>
       </div>
